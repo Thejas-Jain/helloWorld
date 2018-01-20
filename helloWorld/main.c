8156921688009f0af4bb9c385c3dad2557f0de22
Thejas Jain <thejas.2409@gmail.com>
2018-01-20 14:12:12 +0530

    added player strength

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -5,6 +5,7 @@ int main()
     printf("Welcome to Zathura game\n");
     printf("choose a player\n");
     printf("1. anda\n2.bonda\n3. conda\n");
+    printf("100\n70\n85\n");
     printf("i'm from development\n");
     printf("I have root access\n");
     return 0;
