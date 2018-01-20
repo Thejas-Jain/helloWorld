1676763bb8d13b74ab3dcbc060e9882b3819d204
Thejas Jain <thejas.2409@gmail.com>
2018-01-20 14:13:48 +0530

    a warning to all players

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -6,5 +6,6 @@ int main()
     printf("choose a player\n");
     printf("1. anda\n2.bonda\n3. conda\n");
     printf("i'm from development\n");
+    printf("I have root access\n");
     return 0;
 }
