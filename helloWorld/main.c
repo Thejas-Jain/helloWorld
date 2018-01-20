265021a9051d3de3c37b3daf52db7aed065f9dff
Thejas Jain <thejas.2409@gmail.com>
2018-01-20 13:17:03 +0530

    added player name

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -4,5 +4,6 @@ int main()
     printf("Hello world, amigos!\n");
     printf("Welcome to Zathura game\n");
     printf("choose a player\n");
+    printf("1. anda\n2.bonda\n3. conda\n");
     return 0;
 }
